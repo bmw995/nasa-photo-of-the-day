f8e6925cc2ac9c80e7ed7c572a5a61844b0887b2
unknown <vimmer995@gmail.com>
2020-03-04 17:48:20 -0500

    trying something

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
+import Date from "./components/Date";
+import FetchedPhoto from "./components/FetchedPhoto.js";
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+class App extends Component {
+  state = {
+    date: "",
+    PhotoFromapi: ""
+  };
+
+  updateDate = e => {
+    e.preventDefault();
+    let dateFromInput = e.target[0].value;
+    this.setState({ date: dateFromInput });
+    this.FetchPhotoFromApi(dateFromInput);
+  };
+
+  FetchPhotoFromApi = date => {
+    fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=PYaXjEGiQ8hZinDbYCldOqD9NYmzfi8Vy26kOIwd`)
+      .then(response => response.json())
+      .then(photoData => this.setState({ photo: photoData }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>NASA's Picture of the Day</h1>
+        <Date updateDate={this.updateDate} />
+        <FetchedPhoto photo={this.state.photo} />
+      </div>
+    );
+  }
 }
 
 export default App;
