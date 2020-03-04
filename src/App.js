fc37642e6754adbde4485895bb7fc22c44851977
unknown <vimmer995@gmail.com>
2020-03-04 18:31:10 -0500

    redid assignment i think this is better

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-import React, { Component } from "react";
-import Date from "./components/Date";
-import FetchedPhoto from "./components/FetchedPhoto.js";
+import React, {useEffect, useState} from "react";
+import "./App.css";
+import NasaData from "./components/NasaData"
 
-class App extends Component {
-  state = {
-    date: "",
-    PhotoFromapi: ""
-  };
 
-  updateDate = e => {
-    e.preventDefault();
-    let dateFromInput = e.target[0].value;
-    this.setState({ date: dateFromInput });
-    this.FetchPhotoFromApi(dateFromInput);
-  };
+function App() {
+  return (
+    <div className="App">
 
-  FetchPhotoFromApi = date => {
-    fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=PYaXjEGiQ8hZinDbYCldOqD9NYmzfi8Vy26kOIwd`)
-      .then(response => response.json())
-      .then(photoData => this.setState({ photo: photoData }));
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>NASA's Picture of the Day</h1>
-        <Date updateDate={this.updateDate} />
-        <FetchedPhoto photo={this.state.photo} />
-      </div>
-    );
-  }
+    <NasaData/>
+    </div>
+  );
 }
 
 export default App;
